767c74acb1faff885a4665bc89aca69279a1563b
david Karanja <davidkaranja@davids-MacBook-Pro.local>
2024-08-22 10:45:02 +0300

    Completed Api Service

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 const connectDB = require("./config/db");
@@ -14,6 +15,14 @@ app.use(express.static(path.join(__dirname, "public")));
 // Body Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// cors middleware
+app.use(
+  cors({
+    origin: ["http://localhost:8000", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 //  app route
 app.get("/", (req, res) => {
