5c8de05a3d9c0450b478268faf5641bd5b76b7a5
david Karanja <davidkaranja@davids-MacBook-Pro.local>
2024-08-26 10:27:22 +0300

    Completed saving username to localstorage

--- client/src/components/IdeaForm.js
+++ client/src/components/IdeaForm.js
@@ -14,6 +14,19 @@ class IdeaForm {
   async handleSubmit(e) {
     e.preventDefault();
 
+    // Validation
+    if (
+      !this._form.elements.text.value ||
+      !this._form.elements.tag.value ||
+      !this._form.elements.username.value
+    ) {
+      alert("Please enter all fields");
+      return;
+    }
+
+    // Save user to local storage
+    localStorage.setItem("username", this._form.elements.username.value);
+
     const idea = {
       text: this._form.elements.text.value,
       tag: this._form.elements.tag.value,
@@ -31,6 +44,8 @@ class IdeaForm {
     this._form.elements.tag.value = "";
     this._form.elements.username.value = "";
 
+    this.render();
+
     // Closing the modal after submit from the Modal
     document.dispatchEvent(new Event("closemodal"));
   }
@@ -40,7 +55,9 @@ class IdeaForm {
     <form id="idea-form">
   <div class="form-control">
     <label for="idea-text">Enter a Username</label>
-    <input type="text" name="username" id="username" />
+    <input type="text" name="username" id="username" value ="${
+      localStorage.getItem("username") ? localStorage.getItem("username") : ""
+    }" />
   </div>
   <div class="form-control">
     <label for="idea-text">What's Your Idea?</label>
